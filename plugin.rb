ea4ac72d2eb745d6962b946612a5652e71d24a5b
Marcus Baw <marcusbaw@gmail.com>
2019-05-01 15:11:41 +0100

    further development

--- plugin.rb
+++ plugin.rb
@@ -1,31 +1,74 @@
-# name: Watch Category
-# about: Watches a category for all the users in a particular group
-# version: 0.2
-# authors: Arpit Jalan (original author), adapted by Marcus Baw for Digital Health Networks
-# url: https://github.com/discourse/discourse-watch-category-mcneel
+# name: WatchMutePlugin
+# about: WatchMutePlugin - Watches or Mutes a category for all the users in a particular group
+# version: 0.4
+# authors: Adapted by Marcus Baw for Digital Health Networks, Arpit Jalan (original author)
+# url: https://github.com/pacharanero/discourse-categorywatcher-digitalhealth
 
-module ::WatchCategory
-  def self.watch_category!
-    nz_network_category = Category.find_by_slug("nz-network")
-    nz_network_group = Group.find_by_name("nz-network")
+enabled_site_setting :watch_mute_enabled
+enabled_site_setting :watch_mute_frequency_hours
 
-    unless nz_network_category.nil? || nz_network_group.nil?
-      nz_network_group.users.each do |user|
-        watched_categories = CategoryUser.lookup(user, :watching).pluck(:category_id)
-        CategoryUser.set_notification_level_for_category(user, CategoryUser.notification_levels[:watching], nz_network_category.id) unless watched_categories.include?(nz_network_category.id)
+add_admin_route 'watch-mute.title', 'watch-mute'
+
+PLUGIN_NAME ||= "watch_mute".freeze
+
+module ::WatchMute
+  # move this out into its own file as it will be needed by the tests
+  WATCHES_MUTES = {
+    "Caffe-Inf-members" => {
+      "watch" => ["gncr-book-group"],
+      "mute" => ["atrium-public-area", "events"]
+    },
+  }
+  def self.watch_mute_categories!
+    WATCHES_MUTES.each do |group_name, data|              # iterate over the WATCHES_MUTES constant
+      if group = Group.find_by_name(group_name)     # only if the group is valid
+        group.users.each do |user|                  # iterate over all the users in the group
+            # set the Watches
+            data["watch"].each do |category_name|
+              category_id = Category.where(slug: category_name).pluck(:id).first
+              #Rails.logger.info "#{category_id}, #{category_name}, #{}user, #{CategoryUser.notification_levels[:watching]}"
+              CategoryUser.set_notification_level_for_category(
+                user, CategoryUser.notification_levels[:watching], category_id) unless CategoryUser.exists?(user_id: user.id, category_id: category_id)
+                # if there's already a preference don't override it
+            end
+            # set the Mutes
+            data["mute"].each do |category_name|
+              category_id = Category.where(slug: category_name).pluck(:id).first
+              CategoryUser.set_notification_level_for_category(
+                user, CategoryUser.notification_levels[:muted], category_id) unless CategoryUser.exists?(user_id: user.id, category_id: category_id)
+                # if there's already a preference don't override it
+            end
+          end
+      else
+        Rails.logger.warn "WatchMutePlugin: The group #{group_name} was not found on this server"
+        # log that the group was not found
       end
     end
   end
 end
 
 after_initialize do
-  module ::WatchCategory
-    class WatchCategoryJob < ::Jobs::Scheduled
-      every 1.day
+  module ::WatchMute
+
+    class Engine < ::Rails::Engine
+      engine_name PLUGIN_NAME
+      isolate_namespace WatchMute
+      Rails.logger.info 'WatchMutePlugin: The WatchMute Rails Engine was initialized'
+    end
+
+    class WatchMuteJob < ::Jobs::Scheduled
+      every SiteSetting.watch_mute_frequency_hours.hours
+      Rails.logger.info "WatchMutePlugin: The WatchMuteJob scheduled job has been set up to run every #{SiteSetting.watch_mute_frequency_hours} hours"
 
       def execute(args)
-        WatchCategory.watch_category!
+        Rails.logger.info 'WatchMutePlugin: The WatchMuteJob scheduled job ran'
+        WatchMute.watch_mute_categories!
       end
     end
   end
+
+  Discourse::Application.routes.append do
+    get '/admin/plugins/watch-mute' => 'admin/plugins#index', constraints: StaffConstraint.new
+  end
+
 end
