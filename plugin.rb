62f33c8284baf9945976e97c776d5238f3ddab8c
Marcus Baw <marcusbaw@gmail.com>
2019-05-08 20:01:35 +0100

    refactor to change the way category mutes/watches are set, and to enable tag watching

--- plugin.rb
+++ plugin.rb
@@ -1,8 +1,8 @@
-# name: WatchMutePlugin
-# about: WatchMutePlugin - Watches or Mutes a category for all the users in a particular group
-# version: 0.5
-# authors: Adapted by Marcus Baw for Digital Health Networks, Arpit Jalan (original author)
-# url: https://github.com/pacharanero/discourse-categorywatcher-digitalhealth
+# name: discourse-watchmute
+# about: discourse-watchmute - Watches or Mutes a category for all the users in a particular group
+# version: 0.6.0
+# authors: Adapted by Marcus Baw for Digital Health Networks, original author Arpit Jalan
+# url: https://github.com/pacharanero/discourse-watchmute
 
 enabled_site_setting :watch_mute_enabled
 enabled_site_setting :watch_mute_frequency_hours
@@ -11,41 +11,68 @@ PLUGIN_NAME ||= "watch_mute".freeze
 
 module ::WatchMute
 
-  # TODO: move this out into its own file as it will be needed by the tests
+  # TODO: move this out into its own file
+
   WATCHES_MUTES = {
     "nz-ciln" => {
-      "watch" => ["nz-network"],
-      "mute" => ["faculty-of-clinical-informatics-open-channel", "open-forum", "public"]
+      "nz-network" => :watching_first_post,
+      "faculty-of-clinical-informatics-open-channel" => :muted,
+      "open-forum" => :muted,
+      "public" => :muted,
+      "nz" => :watching_first_post,
+      "nz-important" => :watching,
     },
     "nz-dig-leaders" => {
-      "watch" => ["nz-network"],
-      "mute" => ["faculty-of-clinical-informatics-open-channel", "open-forum", "public"]
+      "nz-network" => :watching_first_post,
+      "faculty-of-clinical-informatics-open-channel" => :muted,
+      "open-forum" => :muted,
+      "public" => :muted,
+      "nz" => :watching_first_post,
+      "nz-important" => :watching,
     },
+    # "Caffe-Inf-members" => {
+    #   "nz-network" => :watching_first_post,
+    #   "faculty-of-clinical-informatics-open-channel" => :muted,
+    #   "open-forum" => :muted,
+    #   "public" => :muted,
+    #   "nz" => :watching_first_post,
+    #   "nz-important" => :watching,
+    # },
   }
 
   def self.watch_mute_categories!
-    WATCHES_MUTES.each do |group_name, data|              # iterate over the WATCHES_MUTES constant
+    WATCHES_MUTES.each do |group_name, data|        # iterate over the WATCHES_MUTES constant
       if group = Group.find_by_name(group_name)     # only if the group is valid
         group.users.each do |user|                  # iterate over all the users in the group
-            # set the Watches
-            data["watch"].each do |category_slug|
-              category_id = Category.where(slug: category_slug).pluck(:id).first
-              #Rails.logger.info "#{category_id}, #{category_name}, #{}user, #{CategoryUser.notification_levels[:watching]}"
+          # set the Watches
+          data.each do |slug, desired_notification_level|
+            # assume the slug is a Category, but try Tags next
+            if category_id = Category.where(slug: slug).pluck(:id).first
+
               CategoryUser.set_notification_level_for_category(
-                user, CategoryUser.notification_levels[:watching], category_id) unless CategoryUser.exists?(user_id: user.id, category_id: category_id)
+                user, CategoryUser.notification_levels[desired_notification_level], category_id) unless CategoryUser.exists?(user_id: user.id, category_id: category_id)
                 # 'unless' means if there's already a preference we don't override it
-            end
-            # set the Mutes
-            data["mute"].each do |category_slug|
-              category_id = Category.where(slug: category_slug).pluck(:id).first
-              CategoryUser.set_notification_level_for_category(
-                user, CategoryUser.notification_levels[:muted], category_id) unless CategoryUser.exists?(user_id: user.id, category_id: category_id)
-                # 'unless' means if there's already a preference we don't override it
+                # remove the unless if you want it to overwrite existing prefs (and annoy your users)
+
+            elsif tag_id = Tag.where(name: slug).pluck(:id).first
+              # if it isn't a Category, try looking it up as a Tag
+              Rails.logger.info "discourse-watchmute: The Tag #{slug} was found on this server - watching and muting statuses will now be set"
+
+              TagUser.change(
+                user, tag_id, TagUser.notification_levels[desired_notification_level]) unless TagUser.exists?(user_id: user.id, tag_id: tag_id)
+              # 'unless' means if there's already a preference we don't override it
+              # remove the unless if you want it to overwrite existing prefs (and annoy your users)
+
+            else
+              # the slug isn't a Category OR a Tag - fail but log it.
+              Rails.logger.warn "discourse-watchmute: The Category or Tag #{slug} was not found on this server - watching and muted statuses could not be set"
             end
           end
+        end
+
       else
-        Rails.logger.warn "WatchMutePlugin: The group #{group_name} was not found on this server - watching and muted statues could not be set"
-        # log that the group was not found
+        # else log that the group was not found
+        Rails.logger.warn "discourse-watchmute: The group #{group_name} was not found on this server - watching and muted statuses could not be set"
       end
     end
   end
@@ -57,15 +84,15 @@ after_initialize do
     class Engine < ::Rails::Engine
       engine_name PLUGIN_NAME
       isolate_namespace WatchMute
-      Rails.logger.info 'WatchMutePlugin: The WatchMute Rails Engine was initialized'
+      Rails.logger.info 'discourse-watchmute: The WatchMute Rails Engine was initialized'
     end
 
     class WatchMuteJob < ::Jobs::Scheduled
       every SiteSetting.watch_mute_frequency_hours.hours
-      Rails.logger.info "WatchMutePlugin: The WatchMuteJob scheduled job has been set up to run every #{SiteSetting.watch_mute_frequency_hours} hours"
+      Rails.logger.info "discourse-watchmute: The WatchMuteJob scheduled job has been set up to run every #{SiteSetting.watch_mute_frequency_hours} hours"
 
       def execute(args)
-        Rails.logger.info 'WatchMutePlugin: The WatchMuteJob scheduled job ran'
+        Rails.logger.info 'discourse-watchmute: The WatchMuteJob scheduled job ran'
         WatchMute.watch_mute_categories!
       end
     end
